ea9206a33704f599033a5618b0b95137897f4c76
ivanluna01 <145233439+ivanluna01@users.noreply.github.com>
2023-09-19 18:38:22 -0300

    Add files via upload

---
+++ punto 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
+	for (int i=0;i<=9;i++){
+		printf ("\n%d",grados[i]);
+	}
+
+	
+	return 0;
+
+}
+
+
