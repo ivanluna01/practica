e37ef80ca26d0cfcf82806493953ad7521115f7d
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-09-19 19:13:22 -0300

    Cambio de Incremento a Decremento.

--- punto 1.c
+++ punto 1.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
 	int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
-	for (int i=0;i<=9;i--){ //Decremento (Joabriel).
+	for (int i=grados[]-1; i>=0 i--){ //Decremento (Joabriel).
 		printf ("\n%d",grados[i]);
 	}
 
