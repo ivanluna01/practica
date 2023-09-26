8a20506c168425f520af27cae31aa444b9887c1c
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-09-26 18:17:42 -0300

    Agregue el punto 2 en el punto 1.

--- punto 1.c
+++ punto 1.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
+// punto 2.
 int main(int argc, char *argv[]) {
 	int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
-	for (int i=grados[]-1; i>=0 i--){ //Decremento (Joabriel).
-		printf ("\n%d",grados[i]);
+	for (int i=9;i>=0;i--){
+		printf("\n%d",grados[i]);
 	}
-
 	
 	return 0;
-
 }
-
 
