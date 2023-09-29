a4e3ab64c5ef86c664199b01cb0c3d2c216fa6e2
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-09-29 19:23:46 -0300

    messi

--- punto 1.c
+++ punto 1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-//Punto 4.c
+
 int main(int argc, char *argv[]) {
-        int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
-
-        for(int i=0; i<=grados; i**){
-                print(grados[i]);
-        }
-
-
-        return 0;
+	int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
+	int i=1;
+	while (i<=9)
+	{
+		printf("\n%d",grados[i]);i+=2;		
+	}
+	
+	return 0;
 }
 
 
