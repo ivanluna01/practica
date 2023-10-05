16bbfbfa268f5e852bed1b7de4ee07254438abc3
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-10-05 19:10:49 -0300

    Calculadora Python.

---
+++ Calcu.py
@@ -0,0 +1,8 @@
+print ("Calculadora: ")
+a = float (input("Ingrese primer número: "))
+b = input ("Ingrese el operador")
+c = float (input("Ingrese otro número:"))
+if(b=="+"): print(a+c);
+if(b=="-"): print(a-c)
+if(b=="*"): print(a*c)
+if(b=="/"): print(a/c)
