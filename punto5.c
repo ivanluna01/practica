e53315148da3409552b81d15d39d0390336df03b
ivanluna01 <145233439+ivanluna01@users.noreply.github.com>
2023-10-05 18:23:54 -0300

    Add files via upload

---
+++ punto5.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	int grados[10] = {12, 4, -7, 90, 4, 56, 23, -90, 1, 12};
+	
+	printf("Los valores 4, 90, 56, -90 y 12 son:\n");
+	
+	for (int i = 0; i < 10; i++) {
+		if (grados[i] == 4 || grados[i] == 90 || grados[i] == 56 || grados[i] == -90 || grados[i] == 12) {
+			printf("%d\n", grados[i]);
+		}
+	}
+	
+	return 0;
+}
+
+
