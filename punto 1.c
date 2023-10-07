89805be28f32c5c078c6de03e71e601dcfbc4320
Joabriel <Quijanojoaquingabriel@gmail.com>
2023-10-06 21:39:38 -0300

    Punto 6 creo?

--- punto 1.c
+++ punto 1.c
@@ -2,13 +2,12 @@
 
 int main(int argc, char *argv[]) {
 	int grados [10] = { 12, 4, -7, 90, 4, 56, 23, -90, 1, 12 };
-	int i=1;
-	while (i<=9)
-	{
-		printf("\n%d",grados[i]);i+=2;		
+	
+	for(int i=0; i<=sizeof(grados); i++){
+		if("%d ",grados[i]%2==0){
+			printf("%d",grados[i]);
+		}
 	}
 	
 	return 0;
 }
-
-
