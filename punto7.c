36a19b54cfb1cf40d8afde67e580f85bb250bee6
Joabriel <114449820+Joabriel@users.noreply.github.com>
2023-10-08 19:20:59 -0300

    Add files via upload

---
+++ punto7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Definición de la macro N
+
+int main() {
+const int var= 10;
+	
+	// Declarar un vector de tamaño N
+	int vector[var];
+	
+	printf("Ingrese %d valores para poblar el vector:\n", var);
+	
+	// Poblar el vector con valores ingresados por el usuario
+	for (int i = 0; i < var; i++) {
+		printf("Ingrese el valor para la posición %d: ", i);
+		scanf("%d", &vector[i]);
+	}
+	
+	// Mostrar los valores del vector por pantalla
+	printf("Valores del vector:\n");
+	for (int i = 0; i < var; i++) {
+		printf("%d ", vector[i]);
+	}
+	printf("\n");
+	
+	return 0;
+}
