ba8b7cd1ec6c4dbc69ae6aff86c8ad1c3eddb0ef
Joabriel <114449820+Joabriel@users.noreply.github.com>
2023-10-08 20:18:44 -0300

    Add files via upload

--- punto8.c
+++ punto8.c
@@ -1,12 +1,30 @@
 #include <stdio.h>
 
-int main(){
+// Definición de macros para las dimensiones de la matriz
+#define M 3  // Número de filas
+#define N 3  // Número de columnas
 
-    for(int f=0; f<=sizeof(/*(Macro)*/);f++){
-        for(int c=0; c<=sizeof(/*(Macro)*/);c++){
-            printf("%d",/*(Macro[f])*/);
-            printf("%d",/*(Macro[c])*/);
-        }
-    }
-    return 0;
+int main() {
+	int matriz[M][N];
+	
+	printf("Ingrese los valores para la matriz %dx%d:\n", M, N);
+	
+	// Llenar la matriz con valores ingresados por el usuario
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("Ingrese el valor para la posición (%d, %d): ", i , j );
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+	
+	// Mostrar la matriz por pantalla
+	printf("La matriz ingresada es:\n");
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			printf("%d ", matriz[i][j]);
+		}
+		printf("\n");
+	}
+	
+	return 0;
 }
